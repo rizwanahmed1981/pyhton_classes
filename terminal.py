3e00a1980f76c6c91ebbd9acbc0be08d2520317b
rizwanahmed1981 <razykan.ra@gmail.com>
2025-03-01 14:39:25 +0500

    flight checkin function updated

--- terminal.py
+++ terminal.py
@@ -1,5 +1,12 @@
 from functions import *
 
 
-resul = input("Please Enter your Flight budget/ domestic or business : ").lower()
-main_func(resul)
+
+count = int(input("Enter number of people: "))
+for i in range(count):
+    check_in = input("please enter yes or no: ").lower()
+    if check_in == "yes":
+        resul = input("Please Enter your Flight budget/ domestic or business : ").lower()
+        main_func(resul)
+    elif check_in == "no":
+        print("no problem you can ask me todo it any time")
